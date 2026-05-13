036c46ad28bc9cb8bfbbf773b0250e20ea204376
gustavogalioti <gustavowolkerz@gmail.com>
2026-05-12 22:32:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Anthropic = require("@anthropic-ai/sdk");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
